5e0782ef80c637641acd9fe2a5171f158b98adb2
Steamedfish <beierjy@users.noreply.github.com>
2016-08-17 14:27:43 +0800

    Update and rename 2_4_10.h to 2_4_10_NumberOf1InBinary.h

--- 2_4_10_NumberOf1InBinary.h
+++ 2_4_10_NumberOf1InBinary.h
@@ -1,16 +1,19 @@
+//注意：算法移位时候，右移高位添加1，容易形成死循环
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h> 
 int NumberOf1(int n){
 	int count = 0;
-	int flag = 1;
-	while (flag){
+	unsigned int flag = 1;
+	while (flag){ //当flag变成0时候，移动了机器数位数
 		if (n & flag)
 			count++;
 		flag = flag << 1;
 	}
 	return count;
 }
+//当把一个数减去1时候，那么它的最右边那个1变成0
 int NumberOf2(int n){
 	int count = 0;
 	while (n){
