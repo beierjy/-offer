dfb8fbba5c569c6995541584ee5f4ce47526f8f7
Steamedfish <beierjy@users.noreply.github.com>
2016-08-23 09:52:19 +0800

    Update and rename 5_3_35.h to 5_3_35_FirstNotRepeatingChar.h

--- 5_3_35_FirstNotRepeatingChar.h
+++ 5_3_35_FirstNotRepeatingChar.h
@@ -1,3 +1,15 @@
+/*
+找出字符串中第一个只出现一次的字符
+详细描述：
+接口说明
+原型：
+bool FindChar(char* pInputString, char* pChar);
+输入参数：
+char* pInputString：字符串
+输出参数（指针指向的内存区域保证有效）：
+char* pChar：第一个只出现一次的字符
+如果无此字符 请输出'.'
+*/
 #include <iostream>
 
 using namespace std;
