3db0c90b3ca272bb91e6b856e28d456f9fcf6a4a
Steamedfish <beierjy@users.noreply.github.com>
2016-08-23 10:06:38 +0800

    Update and rename 5_3_36.h to 5_3_36_InversePairs.h

--- 5_3_36_InversePairs.h
+++ 5_3_36_InversePairs.h
@@ -1,3 +1,10 @@
+/*在数组中的两个数字，
+如果前面一个数字大于后面的数字，
+则这两个数字组成一个逆序对。
+输入一个数组,求出这个数组中的逆序对的总数P。
+并将P对1000000007取模的结果输出。 即输出P%1000000007
+*/
+//利用归并排序的思想
 #include <iostream>
 #include <cstdlib>
 
@@ -24,37 +31,37 @@ int Inverse(int* numbers,int start,int last){
     return left + right + mergecount;
 }
 int MergeCount(int* numbers,int* start1,int*p,int*start2,int*q){
-    if(numbers == NULL || p== NULL ||q == NULL||start1 == NULL||start2==NULL)
-        return 0;
-    int len = q - start1 + 1;
-    int* c = new int[len];
-    int counts = 0;int pos = len - 1;
+	if(numbers == NULL || p== NULL ||q == NULL||start1 == NULL||start2==NULL)
+	return 0;
+	int len = q - start1 + 1;
+	int* c = new int[len];
+	int counts = 0;int pos = len - 1;
 	int* temp = p;
 	while(p >= start1 && q >= start2){
-            if(*p > *q){
-                c[pos--] = *p;
-                counts += q - start2 + 1;
-                p--;
-            }
-            else{
-                c[pos--] = *q;
-                q--;
-            }
+	    if(*p > *q){
+	        c[pos--] = *p;
+	        counts += q - start2 + 1;
+	        p--;
+	    }
+	    else{
+	        c[pos--] = *q;
+	        q--;
+	    }
 	}
-    while(q >= start2){
-        c[pos--] = *q;
-        q--;
-    }
-    while(p >= start1){
-        c[pos--] = *p;
-        p--;
-    }
-    for(int i = 0;i < len;++i){
-        *start1 = c[i];
-        ++start1;
-    }
-    delete[] c;
-    return counts;
+	while(q >= start2){
+		c[pos--] = *q;
+		q--;
+	}
+	while(p >= start1){
+		c[pos--] = *p;
+		p--;
+	}
+	for(int i = 0;i < len;++i){
+		*start1 = c[i];
+		++start1;
+	}
+	delete[] c;
+	return counts;
 }
 
 void Test(){
