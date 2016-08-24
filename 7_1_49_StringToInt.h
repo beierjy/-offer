f0676cd88750cd3e4fa7ba88d7d86efdf6936294
Steamedfish <beierjy@users.noreply.github.com>
2016-08-24 14:45:38 +0800

    Update and rename 7_1_49.h to 7_1_49_StringToInt.h

--- 7_1_49_StringToInt.h
+++ 7_1_49_StringToInt.h
@@ -1,3 +1,4 @@
+//实现从字符串转换到int型整数
 #include <iostream>
 using namespace std;
 enum Status{kValid,kInvalid};
@@ -8,7 +9,7 @@ long long strtoint(char* str,bool minus){
         if(*str > '0'&& *str < '9'){
             int flog = minus ? -1:1;
             num = num * 10 + flog * (*str - '0');
-            if((!minus && num > 0x7FFFFFFF) || (minus && num > 0x8000000))
+            if((!minus && num > 0x7FFFFFFF) || (minus && num > 0x8000000))//判断是否越界，使用补码表示
             {
                 num = 0;
                 break;
@@ -26,12 +27,12 @@ long long strtoint(char* str,bool minus){
     return num;
 }
 int setatoi(char* str){
-    g_nStatus=kInvalid;
+    g_nStatus=kInvalid;//计算是否为空串
     int num=0;
     if(str != NULL && *str != '\0')
     {
         bool minus = false;
-        if(*str == '+'){
+        if(*str == '+'){//考虑符号
             ++str;
         }
         if(*str == '-')
