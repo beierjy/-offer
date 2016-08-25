ae5ec445549db69b2708bd63a17481ccea3c7fbb
Steamedfish <beierjy@users.noreply.github.com>
2016-08-25 10:20:00 +0800

    Update and rename 8_2_53.h to 8_2_53RegularExpressionsMatching.h

--- 8_2_53RegularExpressionsMatching.h
+++ 8_2_53RegularExpressionsMatching.h
@@ -1,8 +1,14 @@
+/*
+请实现一个函数用来匹配包括'.'和'*'的正则表达式。模式中的字符'.'表示任意一个字符，
+而'*'表示它前面的字符可以出现任意次（包含0次）。 在本题中，匹配是指字符串的所有
+字符匹配整个模式。例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但是与"aa.a"和"ab*a"均不匹配
+*/
 #include <iostream>
 #include <string.h>
 #include <string>
 using namespace std;
 
+//暴力法
 bool match1(char* str1,char* str2){
 	if((str1 == NULL && str2 != NULL)||(str1 != NULL && str2 == NULL))
 		return false;
@@ -70,7 +76,7 @@ bool match1(char* str1,char* str2){
 		return false;
 	return true;
 }
-
+//递归求解
 bool matchCore(char* str,char* pattern);
 bool match(char *str,char* pattern)
 {
